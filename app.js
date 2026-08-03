910d760bc2f58be469906148eb1f7b4448a49ef4
ulimarinaulitina-pixel <ulimarinaulitina@gmail.com>
2026-08-03 13:48:47 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// AER Wallet Dashboard Script
+const button = document.getElementById("connectButton");
+
+button.onclick = async () => {
+
+    if (!window.ethereum) {
+        alert("Please install MetaMask");
+        return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    await provider.send("eth_requestAccounts", []);
+
+    const signer = provider.getSigner();
+
+    const address = await signer.getAddress();
+
+    const balance = await provider.getBalance(address);
+
+    const network = await provider.getNetwork();
+
+    document.getElementById("walletAddress").innerHTML = address;
+
+    document.getElementById("walletBalance").innerHTML =
+        Number(ethers.utils.formatEther(balance)).toFixed(4) + " ETH";
+
+    document.getElementById("networkName").innerHTML =
+        network.name + " (" + network.chainId + ")";
+};
